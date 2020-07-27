c523d76f4400e064cfc4c6d40123bb183eebf619
Wojciech Olejnik <wojciech.olejnik@gamehag.com>
2020-07-27 09:57:41 +0200

    Added Angular Material

--- serials-search/src/app/app.module.ts
+++ serials-search/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SerialsComponent } from './pages/serials/serials.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SerialsComponent } from './pages/serials/serials.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
