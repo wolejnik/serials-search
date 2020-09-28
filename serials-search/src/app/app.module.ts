cc7ab484990aadb0da85f3024d1c301d0c1d1be7
Wojciech Olejnik <wojciech.olejnik@gamehag.com>
2020-09-28 21:15:10 +0200

    Added a store devtools

--- serials-search/src/app/app.module.ts
+++ serials-search/src/app/app.module.ts
@@ -19,7 +19,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { SerialComponent } from './pages/serial/serial.component';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
-import { serialsReducer } from './store/reducers/serials.reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { reducers } from './store/reducers';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,12 @@ import { serialsReducer } from './store/reducers/serials.reducers';
     MatPaginatorModule,
     MatSelectModule,
     HttpClientModule,
-    StoreModule.forRoot({ serials: serialsReducer }),
+      StoreModule.forRoot(reducers),
+      // Instrumentation must be imported after importing StoreModule (config is optional)
+      StoreDevtoolsModule.instrument({
+        maxAge: 25, // Retains last 25 states
+        // logOnly: environment.production, // Restrict extension to log-only mode
+      }),
   ],
   providers: [RequestService, ColorRatingService],
   bootstrap: [AppComponent],
